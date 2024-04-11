249ce94b8e2e887ed9142cacb1aa4377d69f4fe6
jsump <libbyn79@gmail.com>
2024-04-11 08:22:05 +0000

    Edit

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -16,7 +16,6 @@ def filter_datum(fields, redaction, message, separator):
     pattern = re.compile(
             r'({})=([^{}]+)'.format('|'.join(fields), separator))
     return pattern.sub(
-            lambda m: m.group(1) + '=' + \
-                    redaction * min(1, len(m.group(2))),
+            lambda m: m.group(1) + '=' + redaction * min(1, len(m.group(2))),
             message
         )
