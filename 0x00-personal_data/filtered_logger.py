c121b9e02fdcda542b162c48bc62161090f66a44
jsump <libbyn79@gmail.com>
2024-04-11 08:41:56 +0000

    Edit

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,7 +10,15 @@ import re
 
 
 def filter_datum(fields, redaction, message, separator):
-    """This method returns the message obfuscated"""
+    """
+    This method returns the message obfuscated
+
+    fields: list of strings representing fields to obfuscate
+    redaction: string representing by what firls will be obfucated
+    message: string representing the log line
+    separator: string representing by which character is separating
+    all fiends in the log line(message)
+    """
     new_msg = re.sub(
             r'({})=([^{}]+)'.format('|'.join(fields), separator),
             lambda m: m.group(1) + '=' + redaction * min(1, len(m.group(2))),
