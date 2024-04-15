bea95ac210c6984449e31412fc7176014e317a91
jsump <libbyn79@gmail.com>
2024-04-15 23:31:16 +0300

    Unauthorized requests

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,9 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/api/v1/unauthorized', methods=['GET'])
+def unauthorized_endpoint() -> str:
+    """ GET /api/v1/status and abort"""
+    abort(401)
