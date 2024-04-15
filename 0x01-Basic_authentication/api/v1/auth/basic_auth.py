24cac754a6934173e38af418a276cd3e8bf65b8e
jsump <libbyn79@gmail.com>
2024-04-15 21:29:35 +0300

    Return user based on email and password

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,7 +7,9 @@ This module contains a class that inherits from Auth
 
 
 import base64
+from models.user import User
 from api.v1.auth.auth import Auth
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -53,3 +55,24 @@ class BasicAuth(Auth):
             return None, None
         email, password = decoded_base64_authorization_header.split(':', 1)
         return email, password
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """
+        This method returns the user instance based on
+        the email and password
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        try:
+            user = User.search({'email': user_email})
+        except User.DoesNotExist:
+            return None
+
+        if not user or not user[0].is_valid_password(user_pwd):
+            return None
+
+        return user[0]
