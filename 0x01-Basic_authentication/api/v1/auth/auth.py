d09935b0edf506d231a4ec86bd84f211c9da3c0c
jsump <libbyn79@gmail.com>
2024-04-15 16:51:07 +0300

    Authenticate class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Module: auth.py
+This module contains a class for authentication
+"""
+
+
+from flask import request
+from typing import List, TypeVar
+
+
+User = TypeVar('User')
+
+
+class Auth:
+    """
+    This class contains methods on authentication
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        THis method takes the path and exclused_paths as parameters
+        Returns: False
+        """
+        return False
+    
+    def auth_header(self, request=None) -> str:
+        """
+        This method takes an optional request parameter
+        of flask bject header and returns None
+        """
+        return None
+    
+    def curr_user(self, request=None) -> TypeVar('User'):
+        """
+        This method takes in the current user as a parameter
+        Return: None
+        """
