21ca9f7b793d4eaabb689004d841ef01a3843447
jsump <libbyn79@gmail.com>
2024-04-25 16:29:24 +0300

    Foundation for restart

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -55,6 +55,7 @@ def login():
     else:
         abort(401)
 
+
 @app.route('/sessions', methods=['DELETE'])
 def logout():
     """
@@ -64,7 +65,7 @@ def logout():
     user = get_user_from_session_id(session_id)
 
     if user is not None:
-        destroy_session(user,id)
+        destroy_session(user.id)
         return redirect('/')
     else:
         abort(403)
@@ -97,9 +98,29 @@ def get_reset_password_token():
         token = str(uuid.uuid4())
         update_reset_token(user.id, token)
 
-        return jsonify({"email": "<user email>", "reset_token": "<reset token>"})
+        return jsonify(
+            {"email": "<user email>", "reset_token": "<reset token>"})
     else:
         abort(403)
+
+
+@app.route('/reset_password', methods=['PUT'])
+def update_password():
+    """
+    This method updaes the password
+    """
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+
+    try:
+        if validate_reset_token(reset_token):
+            response = {"email": "<user email>", "message": "Password updated"}
+            return jsonify(response), 200
+        else:
+            return "Invalid token", 403
+    except Exception as e:
+        return str(e), 403
 
 
 if __name__ == "__main__":
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,6 +6,7 @@ Authentication file
 
 import bcrypt
 import uuid
+import hashlib
 from sqlalchemy.orm.exc import NoResultFound
 from typing import Any
 from db import DB
@@ -96,7 +97,7 @@ class Auth:
         This method destroys the session
         """
         self._db.update_session_id(user_id, None)
-    
+
     def get_reset_password_token(self, eail: str) -> str:
         """
         This method generated a UUID and update for user's
@@ -109,10 +110,16 @@ class Auth:
         token = str(uuid.uuid4())
         self._db.update_reset_token(user.id, token)
         return token
-    
+
     def update_password(self, reset_token: str, password: str) -> None:
         """
         This method updates password
         """
         user = self._db.get_user_by_reset_token(reset_token)
-        
+
+        if user is None:
+            raise ValueError("User does not exist")
+
+        hashed_password = hashlib.sha256(password.encode()).hexdigest()
+        self._db.update_password_And_reset_Token(
+            user.id, hashed_password, None)
