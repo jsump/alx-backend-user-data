de7f37bd8c546beb8520b89b0e2fc24685059bf2
jsump <libbyn79@gmail.com>
2024-04-25 10:58:25 +0000

    Regiyser user with flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -9,11 +9,29 @@ from flask import Flask, jsonify
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/')
 def index():
+    """
+    this method returns the message
+    """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('users', methods=['POST'])
+def users():
+    """
+    This method implements the endpoint to register a user
+    """
+    email = request.formget('email')
+    password = request.form.get('password')
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "user email already registeres"}), 400
 
 
 if __name__ == "__main__":
