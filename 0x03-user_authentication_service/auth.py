cc5183642b88d9e0eb182f9d17635f91bf5a38c5
jsump <libbyn79@gmail.com>
2024-04-25 22:56:57 +0300

    Task 10 edit

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -72,10 +72,12 @@ class Auth:
         This method creates a new session
         """
         user = self._db.find_user_by(email)
-        session_id = str(uuid.uuid4())
-        self._db.store_session_id(user.id, session_id)
-
-        return session_id
+        if user:
+            session_id = str(uuid.uuid4())
+            self._db.store_session_id(user.id, session_id)
+            return session_id
+        else:
+            raise ValueError("User not found")
 
     def get_user_from_session_id(self, session_id: str):
         """
