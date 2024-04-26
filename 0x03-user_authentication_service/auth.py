ce754fcbca8aaaeacd6ba3fa2a7f47d7d443628b
jsump <libbyn79@gmail.com>
2024-04-26 10:11:37 +0300

    Task 9 check

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -60,7 +60,7 @@ class Auth:
             return True
         return False
 
-    def _generate_uuid() -> str:
+    def _generate_uuid(self, new_uuid) -> str:
         """
         This method generates a new UUID
         """
